d9913f161d5b848de5737cd5540b83a0fac1d542
YevheniiBiloshapka <106679993+YevheniiBiloshapka@users.noreply.github.com>
2022-09-02 15:38:10 +0300

    add homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+/* Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
 
-console.log(galleryItems);
+*1. Добавь библиотеку SimpleLightbox как зависимость проекта используя npm (ссылка на CDN из твоей прошлой работы больше не нужна).
+*2. Используй свой JavaScript код из предыдущей домашней работы, но выполни рефакторинг с учетом того, что библиотека была установлена через npm (синтаксис import/export).
+*3. Для того чтобы подключить CSS код библиотеки в проект, необходимо добавить еще один импорт, кроме того который описан в документации.*/
+
+const galleryBox = document.querySelector('.gallery');
+
+const cardImg = createImgCard(galleryItems);
+galleryBox.insertAdjacentHTML('beforeend', cardImg);
+
+function createImgCard(items) {
+  return items
+    .map(({ preview, description, original }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"  />
+</a>`;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: `alt`,
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+/* Выполняй это задание в файлах 03-feedback.html и 03-feedback.js. 
+Разбей его на несколько подзадач:
 
+*1.Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект
+*с полями email и message, в которых сохраняй текущие значения полей формы. 
+*Пусть ключом для хранилища будет строка "feedback-form-state".
+2.При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, 
+заполняй ими поля формы. В противном случае поля должны быть пустыми.
+
+*3.При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, 
+*message и текущими их значениями в консоль.
+
+*4.Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. 
+*Для этого добавь в проект и используй библиотеку lodash.throttle. */
+
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const isButtonActive = document.querySelector('button');
+const STORAGE_KEY = 'feedback-form-state';
+
+const localData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+console.log(localData);
+
+feedbackForm.email.value = localData.email ?? '';
+feedbackForm.message.value = localData.message ?? '';
+
+isButtonActive.disabled = true;
+
+feedbackForm.addEventListener('input', throttle(setFormData, 500));
+function setFormData(e) {
+  isButtonActive.disabled = false;
+  localData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(localData));
+}
+
+feedbackForm.addEventListener('submit', onClickFormSubmit);
+function onClickFormSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+  console.log(localData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  isButtonActive.disabled = true;
+}
